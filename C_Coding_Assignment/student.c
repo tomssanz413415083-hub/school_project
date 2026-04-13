567671b17c5c9a99bceabb780d1ce6b820341bd0
Tommy-Sanjaya123 <tomssanz413415083@gmail.com>
2026-04-14 00:30:10 +0800

    add search student by ID feature

--- C_Coding_Assignment/student.c
+++ C_Coding_Assignment/student.c
@@ -36,3 +36,26 @@ void displayStudents(Student students[], int count) {
                students[i].score);
     }
 }
+
+void searchStudent(Student students[], int count) {
+    char searchId[20];
+    int i, found = 0;
+
+    printf("Enter student ID to search: ");
+    scanf("%s", searchId);
+
+    for (i = 0; i < count; i++) {
+        if (strcmp(students[i].id, searchId) == 0) {
+            printf("Student found:\n");
+            printf("Name: %s\n", students[i].name);
+            printf("ID: %s\n", students[i].id);
+            printf("Score: %.2f\n", students[i].score);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("Student not found.\n");
+    }
+}
